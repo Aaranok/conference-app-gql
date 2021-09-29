122e4f6759bfc9400e2702e1522d97778eb9d13e
Vlad Iacobescu <55537680+Aaranok@users.noreply.github.com>
2021-09-29 17:09:38 +0300

    wed_1

--- src/startup/dataLoaders.js
+++ src/startup/dataLoaders.js
@@ -1,5 +1,9 @@
 const { getUserDataLoaders } = require('../features/user/dataLoaders')
+const { getConferenceLoaders } = require('../features/conference/dataLoader')
+const { getDictionaryLoaders } = require('../features/dictionary/dataLoaders')
 
 module.exports = dbInstance => ({
-  ...getUserDataLoaders(dbInstance)
+  ...getUserDataLoaders(dbInstance),
+  ...getConferenceLoaders(dbInstance),
+  ...getDictionaryLoaders(dbInstance)
 })
